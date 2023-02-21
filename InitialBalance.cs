22b6784b6f57fcfa873c7d7e37d49640718c9c3c
Gem Immanuel <gemify@gmail.com>
2023-02-21 12:29:41 -0500

    Removed unused using references

--- InitialBalance.cs
+++ InitialBalance.cs
@@ -1,29 +1,17 @@
 #region Using declarations
+using NinjaTrader.Cbi;
+using NinjaTrader.Data;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.NinjaScript.DrawingTools;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Xml.Serialization;
-using NinjaTrader.Cbi;
-using NinjaTrader.Gui;
-using NinjaTrader.Gui.Chart;
-using NinjaTrader.Gui.SuperDom;
-using NinjaTrader.Gui.Tools;
-using NinjaTrader.Data;
-using NinjaTrader.NinjaScript;
-using NinjaTrader.Core.FloatingPoint;
-using NinjaTrader.NinjaScript.DrawingTools;
-using System.Diagnostics;
-using NinjaTrader.FIX;
-using System.Globalization;
-using System.Security.Cryptography;
-using Microsoft.VisualBasic.ApplicationServices;
 #endregion
 
 //This namespace holds Indicators in this folder and is required. Do not change it. 
